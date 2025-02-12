59b48a97cfc5e8520de4357ec2e5bbfa7c701196
Diego Armando <dcutivao@gmail.com>
2025-02-12 11:11:55 -0500

    Ajuste output principal

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,11 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
-
-output "iam_instance_profile" {
-  value = module.iam.iam_instance_profiles[*]
-}
 output "bucket_arn" {
   value = module.s3_bucket.bucket_arn[*]
+}
+
+output "rds_endpoint" {
+  description = "Enpoint conexion DB"
+  value = module.rds.rds_endpoint
 }
